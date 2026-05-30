f2402d0467b234d7ff738bd4a7076d1d1bb9dee9
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-05-30 16:02:37 +0000

    music bot update

---
+++ bot.py
@@ -0,0 +1,24 @@
+from pyrogram import Client
+from config import *
+
+bot = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+assistant = Client(
+    "assistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
+)
+
+assistant.start()
+bot.start()
+
+print("Bot Started")
+print("Assistant Started")
+
+bot.idle()
